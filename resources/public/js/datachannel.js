0bdb5c2ea6e52d199d3a1b0fd93a516d42cfa2f3
Henry Clifford <h.a.clifford@gmail.com>
2013-01-04 17:41:13 -0500

    resolving gh-pages issue

--- resources/public/js/datachannel.js
+++ resources/public/js/datachannel.js
@@ -53,7 +53,6 @@ function DCPF_install(ws_url)
     this.close = function(){this._udt.close()}
     // BUGFIX: undefined onerror causes issues in some chrome versions
     this.send  = function(data, onerror){
-      if(!onerror) onerror = function(){};
       this._udt.send(data);
     }
 
