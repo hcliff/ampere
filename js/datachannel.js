fbe119cea5624480ccce07b1a8e17770880acf4d
Henry Clifford <h.a.clifford@gmail.com>
2013-01-04 17:58:51 -0500

    update gh-pages

--- js/datachannel.js
+++ js/datachannel.js
@@ -51,11 +51,7 @@ function DCPF_install(ws_url)
     }
 
     this.close = function(){this._udt.close()}
-    // BUGFIX: undefined onerror causes issues in some chrome versions
-    this.send  = function(data, onerror){
-      if(!onerror) onerror = function(){};
-      this._udt.send(data);
-    }
+    this.send  = function(data, onerror){this._udt.send(data, onerror)}
 
     this.readyState = "connecting"
   }
