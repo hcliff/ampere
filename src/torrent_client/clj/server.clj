bce5ec4445b8839c2b3d6c84ba3d3efdb669f31f
Henry Clifford <h.a.clifford@gmail.com>
2013-02-19 23:52:14 -0500

    correct clj namespacing

--- src/torrent_client/clj/server.clj
+++ src/torrent_client/clj/server.clj
@@ -1,11 +1,10 @@
-(ns torrent-client.clj.server
+(ns torrent-client.server
   (:require [noir.server :as server]))
 
-(server/load-views-ns 'torrent-client.views)
+(server/load-views-ns 'views)
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8091"))]
-    ; (cljs/start mode cljs-options)
     (server/start port {:mode mode
                         :ns 'torrent-client})))
